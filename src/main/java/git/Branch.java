54c7d3191781ffe9b203227871c5159632cabcab
AidaiZhumabekovna <zumabekovnaajdaj29@gmail.com>
2023-01-26 16:33:21 +0300

    changed

---
+++ src/main/java/git/Branch.java
@@ -0,0 +1,9 @@
+package git;
+
+public class Branch {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int i = 12;
+        System.out.println(i);
+    }
+}
